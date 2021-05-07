29b23a9399da6e27db73c958d8456c35da41c6e8
changz23-1836696 <43832007+changz23-1836696@users.noreply.github.com>
2021-05-06 20:17:05 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by 曾畅 on 5/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
